0a3c5e25d8c571bf8e17c65e4d925d7e37751a9b
yjjnls <x-jj@foxmail.com>
2018-02-25 16:31:21 +0800

    update message logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,56 @@
 'use strict'
 
 const http = require('http');
+const UUID = require('uuid');
+var message = require('./message.js').msg;
+const msg_state = require('./message.js').msg_state;
 
 http.createServer((req, res) => {
   res.end();
-}).listen(8000); 
+}).listen(8000);
+
+
+function prepare(data, addr) {
+  // create msg
+  let msg = new message(data, addr);
+  var msg_id = UUID.v1();
+  // store msg
+  //todo...
+  // monitor msg
+  msg.on('prepared timeout', () => {
+    // todo...
+  });
+  // return 200 OK
+  // todo...  
+}
+
+function publish(id) {
+  // get msg by id
+  let msg;
+  // todo...
+  // clear prepared timeout
+  msg.Clear_Timeout();
+  // change msg state and monitor msg
+  msg.Change_State(msg_state[1]);
+  msg.SetTimeout();
+  msg.on('published timeout', () => {
+    // todo...
+  });
+  // post msg to MQ
+  // todo...
+  // return 200 OK
+  // todo...
+
+}
+
+function consume(id) {
+  // get msg by id
+  let msg;
+  // todo...
+  // clear published timeout
+  msg.Clear_Timeout();
+  // remove msg
+  // todo...
+  // return 200 OK
+  // todo...
+}
--- src/message.js
+++ src/message.js
@@ -2,10 +2,40 @@
 
 const EventEmitter = require('events').EventEmitter;
 
+const msg_state = ['prepared', 'published', 'consumed'];
+
 class msg extends EventEmitter {
     construct(data, addr) {
-        this.data = data;
-        this.dest_addr = addr;
-
+        // msg data
+        this.data_ = data;
+        // consumer addr
+        this.consumer_addr_ = addr;
+        // msg state
+        this.state_ = msg_state[0];
+        // msg state timeout
+        this.timeout_ = 300;
+        this.timeout_instance_ = Set_Timeout();
     }
+    Change_State(state) {
+        if (msg_state.indexOf(state) == -1) {
+            return false;
+        }
+        this.state_ = state;
+        return true;
+    }
+    Set_Timeout() {
+        var self = this;
+        return setTimeout(() => {
+            self.emit(self.state_ + ' timeout');
+        }, this.timeout_);
+    }
+    Clear_Timeout() {
+        clearTimeout(this.timeout_instance_);
+        this.timeout_instance_ = null;
+    }
+}
+
+module.exports = {
+    msg,
+    msg_state
 }
