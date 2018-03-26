b09ef03c55c7639ae7b72b96df9b96d7ff96f172
yjjnls <x-jj@foxmail.com>
2018-03-26 17:02:29 +0800

    update

--- examples/down_stream.js
+++ examples/down_stream.js
@@ -6,7 +6,7 @@ const url = require('url');
 let request = require('../lib/request.js');
 // var redis = require("redis"),
 //     client = redis.createClient();
-let mq = require('./mq.js');
+let mq = require('../lib/mq.js');
 
 const CRLF = '\r\n';
 function trace(arg) {
@@ -39,62 +39,89 @@ async function subscribe(topic) {
     await mq.subscribe(topic, service_name);
 }
 
-http.createServer((req, res) => {
-    let chunk = [];
+// http.createServer((req, res) => {
+//     let chunk = [];
 
-    req.on('data', function (data) {
-        chunk.push(data);
-    });
+//     req.on('data', function (data) {
+//         chunk.push(data);
+//     });
 
-    req.on('end', function () {
-        let buf = Buffer.concat(chunk);
-        info(buf.toString());
+//     req.on('end', function () {
+//         let buf = Buffer.concat(chunk);
+//         info(buf.toString());
 
-        OnMessage(buf, req, res);
-    })
-}).listen(8002);
-function OnMessage(data, req, res) {
-    if (req.headers) {
-        trace(JSON.stringify(req.headers));
+//         OnMessage(buf, req, res);
+//     })
+// }).listen(8002);
+// function OnMessage(data, req, res) {
+//     if (req.headers) {
+//         trace(JSON.stringify(req.headers));
+//     }
+
+//     let dataStr;
+
+//     if (typeof data !== 'string') {
+//         dataStr = data.toString();
+//     } else {
+//         dataStr = data;
+//     }
+//     let dataObj;//JSON format
+//     try {
+//         trace(dataStr);
+
+//         dataObj = JSON.parse(dataStr);
+//     }
+//     catch (e) {
+//         error('Invalid parameters! Can\'t parse!');
+//         send(res, 400, 'Invalid parameters! Can\'t parse!');
+//         return;
+//     }
+
+//     if (dataObj === undefined) {
+//         error('Invalid parameters! Can\'t parse!');
+//         send(res, 400, 'Invalid parameters! Can\'t parse!');
+//         return;
+//     }
+
+//     let uri = url.parse(req.url, true);
+//     if (uri.pathname == `/${service_name}`) {
+//         // switch (req.method) {
+//         //     case 'POST':
+//         //         add_transcation(res, dataObj);
+//         //         break;
+//         //     case 'GET':
+//         //         query(res, uri.query.id);
+//         //         break;
+//         // }
+//         console.log('no process function!');
+//     }
+//     error('API Not found!');
+//     send(res, 400, 'API Not found!');
+// }
+
+async function consume_msg() {
+    let res = await mq.fetch_msg(service_name);
+    console.log('=====>fetch msg over<=====');
+    for (var i = 0; i < res.length; i = i + 2) {
+        let msg_id = res[i + 1];
+        try {
+            let obj = JSON.parse(res[i]);
+            let data = obj.data;
+            let topic = obj.topic;
+            console.log('-------------------');
+            console.log(`[msg topic] ${topic}\n[msg id] ${msg_id} \n[msg data] ${data} \nDo Something!`);
+            console.log('-------------------');
+
+            await update_recved_msg_id(service_name, topic, msg_id);
+            // notify ras
+            await request.Delete(`http://127.0.0.1:8000/msg?${msg_id}`);
+        } catch (err) {
+            console.log(err.message);
+            throw new Error(err.message);
+        }
     }
-
-    let dataStr;
-
-    if (typeof data !== 'string') {
-        dataStr = data.toString();
-    } else {
-        dataStr = data;
-    }
-    let dataObj;//JSON format
-    try {
-        trace(dataStr);
-
-        dataObj = JSON.parse(dataStr);
-    }
-    catch (e) {
-        error('Invalid parameters! Can\'t parse!');
-        send(res, 400, 'Invalid parameters! Can\'t parse!');
-        return;
-    }
-
-    if (dataObj === undefined) {
-        error('Invalid parameters! Can\'t parse!');
-        send(res, 400, 'Invalid parameters! Can\'t parse!');
-        return;
-    }
-
-    let uri = url.parse(req.url, true);
-    if (uri.pathname == `/${service_name}`) {
-        // switch (req.method) {
-        //     case 'POST':
-        //         add_transcation(res, dataObj);
-        //         break;
-        //     case 'GET':
-        //         query(res, uri.query.id);
-        //         break;
-        // }
-        console.log('no process function!');
-    }
-    error('API Not found!');
-    send(res, 400, 'API Not found!');
 }
+subscribe('money');
+setInterval(async () => {
+    await consume_msg();
+}, 3000);
